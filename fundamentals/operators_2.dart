f27880d730e4e1fd1cc1e259e5a813faba089a84
Felipe <felipe@visionar.io>
2025-05-14 18:30:39 -0300

    add operators

---
+++ fundamentals/operators_2.dart
@@ -0,0 +1,18 @@
+void main() {
+  int a = 10;
+  a = a + 10;
+  a  += 10; // a = a + 10
+  a  -= 10; // a = a - 10
+  a  *= 10; // a = a * 10
+  a  ~/= 10; // a = a ~/ 10
+  a  %= 10; // a = a % 10
+
+  print(a); 
+
+  print(3 > 2); // true
+  print(3 < 2); // false
+  print(3 >= 2); // true
+  print(3 <= 2); // false
+  print(3 == 2); // false
+  print(3 != 2); // true  
+}
