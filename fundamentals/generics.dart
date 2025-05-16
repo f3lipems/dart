12c1992bcebe46ae40315511b5cd435834b17900
Felipe <felipe@visionar.io>
2025-05-16 12:03:18 -0300

    add generics

---
+++ fundamentals/generics.dart
@@ -0,0 +1,16 @@
+main() {
+  print('Início');
+
+  List<String> fruits = ['banana', 'maça', 'laranja'];
+  fruits.add('melão');
+
+  print(fruits);
+
+  Map<String, double> salary = {
+    'gerente': 19345.78,
+    'vendedor': 16345.80,
+    'estagiário': 600.00,
+  };
+
+  print(salary);
+}
